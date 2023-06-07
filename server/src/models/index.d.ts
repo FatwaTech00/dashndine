5c30162635704058bac5c733417daed69483c2ac
Hexagonz <spansel7c@gmail.com>
2023-06-07 09:06:39 +0700

    Menu BackEnd

---
+++ server/src/models/index.d.ts
@@ -0,0 +1,9 @@
+export declare interface Menu {
+    id_menu: string;
+    nama_menu: string;
+    desc_menu: string;
+    harga_menu: number;
+    kategori_menu: string;
+    foto_menu: string
+}
+
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import staffRoute from "./StaffRoute";
+import menuRoute from "./MenuRoute";
 
 const router = express.Router();
 
 export default (): express.Router => {
     staffRoute(router);
-
+    menuRoute(router);
     return router;
 };
