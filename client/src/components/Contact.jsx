f42c56084bae30ad73588c9ad967759d923d43e1
nandaikhwanul <nanda.pertama465@gmail.com>
2023-06-12 22:23:34 +0700

    Penambahan menu

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='absolute w-full h-screen mt-[90em] grid justify-center'>contact</div>
+  )
+}
+
+export default Contact
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
+      colors : {
+        customRed: '#b46c55 '
+      },
       fontFamily: {
         'poppins': ["Poppins", 'sans-serif'],
       },
